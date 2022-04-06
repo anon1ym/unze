fda180098657c300623313fa1aab8d1deda94a2c
anon1ym <102027393+anon1ym@users.noreply.github.com>
2022-04-06 11:38:42 +0530

    Update __main__.py

--- unzipper/__main__.py
+++ unzipper/__main__.py
@@ -12,7 +12,5 @@ if __name__ == "__main__" :
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
     unzipperbot.start()
-    print("Checking Log Channel ...")
-    check_logs()
     print("Bot is active Now! Join @NexaBotsUpdates")
     idle()
